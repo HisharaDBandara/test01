8528f7e758ebc6982ef7d84f71a422dbd5437fa0
hisha <hisha@DESTOP>
2025-02-17 11:20:28 +0530

    Add project files.

---
+++ test01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace outpu
+{
+    class test01
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter Your age: ");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: "+age);
+
+            Console.ReadLine();
+
+            
+            
+        }
+    }
+}
+
+
